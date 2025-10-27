b4cca851cc0b3506fa803dfc62c59f8699361243
dmt041104111003 <daomanhtung4102003@gmail.com>
2025-10-28 02:59:17 +0700

    feat: home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -363,6 +363,11 @@ export default function Header() {
           </div>
 
           <nav className="hidden md:flex space-x-8">
+            <Link href="/" className={getNavLinkClasses('/')}>
+              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg>
+            </Link>
             <Link href="/tin-tuc" className={getNavLinkClasses('/tin-tuc')}>
               Tin tức
             </Link>
